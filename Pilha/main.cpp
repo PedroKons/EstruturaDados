0f6f1c96d3a6643a473db04c0b2d938b921f9a48
Pedro H K Goncalves <108807271+PedroKons@users.noreply.github.com>
2023-04-17 12:00:23 -0300

    Create main.cpp

---
+++ Pilha/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "pilha.h"
+using namespace std;
+
+int main() {
+    TPilha <char> pilha;
+    inicia_pilha(pilha);
+    verif_eq(pilha);
+
+
+   
+
+    return 0;
+
+
+}
